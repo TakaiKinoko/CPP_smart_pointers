b1ea67305721e4980c376db10137eb6c02387354
Fang Han <hanfangviolin@gmail.com>
2018-12-15 19:08:11 -0500

    init

---
+++ CMakeFiles/ptr_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ptr_test.dir/src/ptr_test.cpp.o"
+  "ptr_test.pdb"
+  "ptr_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ptr_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
